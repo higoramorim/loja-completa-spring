b1ab50f709dfe4f21f36fb6367a34100b2f8f930
higoramorim <1810042@aluno.univesp.br>
2021-05-28 21:57:19 -0300

    itempedido itempedidoPK

--- src/main/java/com/higoramorim/cursomc/CursomcApplication.java
+++ src/main/java/com/higoramorim/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.higoramorim.cursomc.domain.Cidade;
 import com.higoramorim.cursomc.domain.Cliente;
 import com.higoramorim.cursomc.domain.Endereco;
 import com.higoramorim.cursomc.domain.Estado;
+import com.higoramorim.cursomc.domain.ItemPedido;
 import com.higoramorim.cursomc.domain.Pagamento;
 import com.higoramorim.cursomc.domain.PagamentoComBoleto;
 import com.higoramorim.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.higoramorim.cursomc.repositories.CidadeRepository;
 import com.higoramorim.cursomc.repositories.ClienteRepository;
 import com.higoramorim.cursomc.repositories.EnderecoRepository;
 import com.higoramorim.cursomc.repositories.EstadoRepository;
+import com.higoramorim.cursomc.repositories.ItemPedidoRepository;
 import com.higoramorim.cursomc.repositories.PagamentoRepository;
 import com.higoramorim.cursomc.repositories.PedidoRepository;
 import com.higoramorim.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -119,6 +124,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
+		
 	}
 
 }
--- src/main/java/com/higoramorim/cursomc/domain/Produto.java
+++ src/main/java/com/higoramorim/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.higoramorim.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ManyToAny;
 
@@ -36,6 +39,9 @@ public class Produto implements Serializable{
 			)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 	}
 
@@ -44,6 +50,14 @@ public class Produto implements Serializable{
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -102,5 +116,13 @@ public class Produto implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 }

3c3ad21eef9209b6076fee0caf5c328ce589f636
higoramorim <1810042@aluno.univesp.br>
2021-05-29 17:47:05 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/higoramorim/cursomc/CursomcApplication.java
+++ src/main/java/com/higoramorim/cursomc/CursomcApplication.java
@@ -70,7 +70,12 @@ public class CursomcApplication implements CommandLineRunner{
 
 		Categoria cat1 = new Categoria(null, "informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Sapato");
+		Categoria cat5 = new Categoria(null, "Calça");
+		Categoria cat6 = new Categoria(null, "Escritório");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -89,7 +94,7 @@ public class CursomcApplication implements CommandLineRunner{
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
--- src/main/java/com/higoramorim/cursomc/services/CategoriaService.java
+++ src/main/java/com/higoramorim/cursomc/services/CategoriaService.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.higoramorim.cursomc.domain.Categoria;
@@ -44,5 +48,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }

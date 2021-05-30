a375d1440c36855d36dd51451a0ac0d0958b639e
higoramorim <1810042@aluno.univesp.br>
2021-05-30 08:10:59 -0300

    ajustando e testando a deleção de cliente

--- src/main/java/com/higoramorim/cursomc/services/ClienteService.java
+++ src/main/java/com/higoramorim/cursomc/services/ClienteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
@@ -56,8 +57,8 @@ public class ClienteService {
 		find(id);
 		try {
 		repo.deleteById(id);
-		} catch(DataIntegrityException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
